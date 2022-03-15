c56639623e3187b5ef608ac635384224d9b6093f
theristoweats <theristoweats@gmail.com>
2022-03-16 00:36:12 +0100

    update

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const server = http.createServer(app);
  
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://eats.theristow.com",
     methods: ["GET", "POST"],
   },
 });
